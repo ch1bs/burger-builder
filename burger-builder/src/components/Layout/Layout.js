c5aa97d9d4cc960fc3c9e36c43f6073d86539d4e
Martin Slavov <martin.slavov@mentormate.com>
2020-09-16 21:07:22 +0300

    added sideDrawer component

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -3,10 +3,12 @@ import React from 'react';
 import Auxiliry from '../../hoc/Auxiliry';
 import classes from '../Layout/Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
   <Auxiliry>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>{props.children}</main>
   </Auxiliry>
 );
