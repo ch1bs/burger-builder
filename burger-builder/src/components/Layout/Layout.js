35f365f0426ac3ef78080f8f2a1517aba5be5bc7
Martin Slavov <martin.slavov@mentormate.com>
2020-09-24 13:40:02 +0300

    reusing the Backdrop Component and adding animation to remove the SideDrawer when clicked

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Auxillary from '../../hoc/Auxillary';
 import classes from '../Layout/Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-  <Auxillary>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>{props.children}</main>
-  </Auxillary>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: true,
+  };
 
-export default layout;
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  render() {
+    return (
+      <Auxillary>
+        <Toolbar />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Auxillary>
+    );
+  }
+}
+export default Layout;
